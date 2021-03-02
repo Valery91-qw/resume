2f48c1e8caf6408a4eaca7549cf22601cefdbc3f
Valery91-qw <vakriv91@gmail.com>
2021-03-02 14:32:17 +0300

    Add async function on form

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -1,21 +1,40 @@
-import React from "react"
+import React, {FormEvent, useState} from "react"
 import style from "./Contact.module.scss"
 import Container from "../common/styles/Container.module.scss"
 import {Fade} from "react-awesome-reveal";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
+type Inputs = {
+    name: string
+    email: string
+    message: string
+}
 
 export function Contact() {
+
+    const { register, handleSubmit, reset , clearErrors ,errors } = useForm<Inputs>()
+
+    const onSubmit = (data: Inputs) => {
+        axios.post(`https://sntp-nodejs-server.herokuapp.com/sendMessage`, data)
+            .then(() => {
+                console.log('Your message ')
+            })
+        //console.log(data)
+        reset()
+    }
     return (
-        <section className={style.contactBox} id="contact">
-            <Fade >
+        <section onClick={() => {clearErrors()}} className={style.contactBox} id="contact">
+            <Fade>
                 <div className={Container.container}>
                     <div className={style.contactContainer}>
                         <h3 className={style.title}>Contact</h3>
-                        <form className={style.form}>
-                            <input className={style.nameForm} placeholder={"Name"}/>
-                            <input className={style.emailForm} placeholder={"email"}/>
-                            <textarea className={style.textarea} placeholder={"Your message"}/>
-                            <button className={style.button}>Отправить</button>
+                        <form onSubmit={handleSubmit(onSubmit)} className={style.form}>
+                            <input className={style.nameForm} placeholder={"Name"} name='name' ref={register}/>
+                            <input className={errors.email ? style.emailFormError :  style.emailForm }
+                                   placeholder={"email"} name='email' ref={register({required: true, pattern: /@/ })}/>
+                            <textarea className={style.textarea} placeholder={"Your message"} name='message' ref={register}/>
+                            <button className={style.button}>send</button>
                         </form>
                     </div>
                 </div>
